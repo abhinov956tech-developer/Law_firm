a2acd6749bd3aff1408b08b3bd189114598eb9a2
Abhinov Dutta <abhinavdutta927@gmail.com>
2024-12-24 23:05:47 +0530

    Commit Done

---
+++ script.js
@@ -0,0 +1,21 @@
+document.getElementById('agree-button').addEventListener('click', function() {
+    document.getElementById('disclaimer-popup').style.display = 'none';
+});
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    const menu = document.getElementById("menu");
+    const menuItems = document.querySelectorAll(".menu-item");
+  
+    menuItems.forEach(item => {
+      item.addEventListener("mouseenter", () => {
+        menuItems.forEach(i => i.classList.remove("active"));
+        item.classList.add("active");
+      });
+    });
+  
+    menu.addEventListener("mouseleave", () => {
+      menuItems.forEach(item => item.classList.remove("active"));
+    });
+  });
+  
